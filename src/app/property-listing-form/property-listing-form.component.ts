a24b52ff6966b9aa819f83f853b807e3b4547390
Jahus1loh <jan@pastucha.pl>
2025-01-06 23:13:51 +0100

    Add propertyListing form

---
+++ src/app/property-listing-form/property-listing-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-listing-form',
+  imports: [],
+  templateUrl: './property-listing-form.component.html',
+  styleUrl: './property-listing-form.component.css'
+})
+export class PropertyListingFormComponent {
+
+}
