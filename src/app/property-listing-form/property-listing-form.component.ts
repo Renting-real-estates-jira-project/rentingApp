81a5ec906a5e279e3f946676e57524b758621ddb
Jahus1loh <jan@pastucha.pl>
2025-01-06 23:10:52 +0100

    Add propertyListing form

--- src/app/property-listing-form/property-listing-form.component.ts
+++ src/app/property-listing-form/property-listing-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {ActivatedRoute, RouteConfigLoadEnd, RouterModule} from '@angular/router';
 import {HousingService} from '../housing.service';
@@ -12,14 +12,21 @@ import {HousingLocation} from '../housinglocation';
   templateUrl: './property-listing-form.component.html',
   styleUrl: './property-listing-form.component.css'
 })
+
 export class PropertyListingFormComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
-  applyForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
+  listingForm = new FormGroup({
+    propertyName: new FormControl(''),
+    propertyCity: new FormControl(''),
+    propertyState: new FormControl(''),
+    propertyPrice: new FormControl(0),
+    propertyPhotos: new FormControl<FileList | null>(null),
+    propertyArea: new FormControl(0),
+    numberOfBedrooms: new FormControl(0),
+    numberOfBathrooms: new FormControl(0, [Validators.required, Validators.min(0)]),
+    wifi: new FormControl(false)
   });
   constructor() {
     const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
@@ -27,11 +34,29 @@ export class PropertyListingFormComponent {
       this.housingLocation = housingLocation;
     });
   }
-  submitApplication() {
-    this.housingService.submitApplication(
-      this.applyForm.value.firstName ?? '',
-      this.applyForm.value.lastName ?? '',
-      this.applyForm.value.email ?? '',
-    );
+
+  handleFileInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length) {
+      this.listingForm.patchValue({ propertyPhotos: input.files });
+      this.listingForm.get('propertyPhotos')?.updateValueAndValidity();
+    }
+  }
+
+  submitApplication(event: Event) {
+    event.preventDefault();
+    if (this.listingForm.valid) {
+      const formData = this.listingForm.value;
+      
+      this.housingService.submitPropertyListing({
+        ...formData,
+        propertyPhotos: formData.propertyPhotos ? Array.from(formData.propertyPhotos) : []
+      });
+
+      console.log('Property listing submitted:', formData);
+      alert('Property listing created successfully!');
+    } else {
+      alert('Please fill all required fields correctly.');
+    }
   }
 }
